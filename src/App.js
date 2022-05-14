478f0f70663312ccd74c3282e65116df20b38d70
DalexisValencia <d.alexis.valencia@gmail.com>
2022-05-14 15:19:07 -0500

    Set up routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './views/search';
+import Details from './views/details';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Search/>}></Route>
+        <Route path='/items/:id”' element={<Details/>}></Route>
+        
+      </Routes>
+    </Router>
   );
 }
 
