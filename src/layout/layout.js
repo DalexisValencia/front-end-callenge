aabe65f74f70eea108fb8c968b7d7316534c7dcd
DalexisValencia <d.alexis.valencia@gmail.com>
2022-05-14 15:41:27 -0500

    Creating a Layout container to wrapp ALL elements into routing

---
+++ src/layout/layout.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+        Aqui va el navegador
+        <Outlet />
+        </>
+    )
+}
+
+export default Layout;
