41a26bbb7bd0a1720b2154842d72c0be3123082b
DalexisValencia <d.alexis.valencia@gmail.com>
2022-05-15 16:57:09 -0500

    Agregando icon shiping

--- src/components/searchCard/searchCard.js
+++ src/components/searchCard/searchCard.js
@@ -1,24 +1,25 @@
 import { Link } from "react-router-dom";
 import './searchCard.scss';
+import c_shipping from '../../assets/misc/ic_shipping.png';
 
-const SearchCard = () => {
+const SearchCard = (props) => {
     return (
-        <Link className="result-card" to="/">
+        <Link className="result-card" to={`/items/${props.item.id}`}>
             <div className="result-card__image">
-                <img src="https://http2.mlstatic.com/D_NQ_NP_689011-MCO45520838216_042021-V.webp" alt="" />
+                <img src={props.item.picture} alt="" />
             </div>
 
             <div className="result-card__content-columns">
                 <div className="result-card__content-wrapper">
                     <h2 className="result-card__price">
                         <span className="result-card__price-symbol">$</span>
-                        <span className="result-card__price-fraction">1280</span>
-                        <img src="" alt="" className="result-card__shiping-free"/>
+                        <span className="result-card__price-fraction">{props.item.price.amount} {props.item.price.currency}</span>
+                        {props.item.free_shipping ? <img src={c_shipping} alt="" className="result-card__shiping-free"/> : null}
                     </h2>
-                    <p className="result-card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, voluptatibus alias doloribus tenetur explicabo reprehenderit,</p>
+                    <p className="result-card__description">{props.item.title}</p>
                 </div>
                 <div className="result-card__content-location">
-                    <h3 className='result-card__location-name'>location</h3>
+                    <h3 className='result-card__location-name'>Capital Frederal</h3>
                 </div>
             </div>
         </Link>
