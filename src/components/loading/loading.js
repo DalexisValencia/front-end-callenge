16520a5d3b46b29d815f9441127d35a2078927be
DalexisValencia <d.alexis.valencia@gmail.com>
2022-05-15 14:27:26 -0500

    Create loading component

---
+++ src/components/loading/loading.js
@@ -0,0 +1,11 @@
+import './loading.scss';
+
+const Loading = () => {
+    return (
+        <div className="loading">
+            Estamos procesando tu consulta ...
+        </div>
+    );
+}
+
+export default Loading;
