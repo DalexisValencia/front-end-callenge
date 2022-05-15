c0eeef5a205c1bb5f3102db445a2e12c96b785f0
DalexisValencia <d.alexis.valencia@gmail.com>
2022-05-15 17:00:03 -0500

    Update details view

--- src/views/details/details.js
+++ src/views/details/details.js
@@ -1,29 +1,78 @@
-import './details.scss';
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import QueriesServices from '../../services/queries.service';
 import BreadCrumb from '../../components/breadcrumb/breadcrumb';
+import './details.scss';
+import ErrorAlert from "../../components/error/error";
+import Loading from "../../components/loading/loading";
+import c_shipping from '../../assets/misc/ic_shipping.png';
 
-const details = () => {
-    return ( 
-        <section className='max-width-element details-item'>
-            <BreadCrumb/>
-            <div className='details-item__wrapper'>
+const Details = () => {
+    const [error, setError] = useState('');
+    const [isLoad, setIsLoad] = useState(false);
+    const [item, setItem] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        if(id) {
+            setIsLoad(true);
+            QueriesServices.details(id).then((result) => {
+                setIsLoad(false);
+                if(result.data.error) {
+                     setError(result.data.error);
+
+                }else if(!result.data.error) {
+                    setItem(result.data.body.item);
+                }
+                },
+                (error) => {
+                    setError(error.message);
+                    setIsLoad(false);
+                }
+            );
+        }else {
+            setIsLoad(false);
+            setError("No existen parámetros de búsqueda");
+        }
+    }, []);
+
+    function TranslateItemStatus(status){
+        switch (status) {
+            case "new":
+                return "Nuevo";
+        }
+    }
+
+    function renderError() {
+        return <ErrorAlert message={error}/>;
+    }
+
+    function renderLoading() {
+        return <Loading />;
+    }
+
+    function renderDetail() {
+        return (
+            <>
                 <div className='details-item__summary'>
                     <div className='details-item__image-wrapper'>
                         <figure className='details-item__figure'>
-                            <img src="https://http2.mlstatic.com/D_NQ_NP_624984-MCO46185694732_052021-O.webp" alt="" />
+                            <img src={item.picture} alt={item.title} />
                         </figure>
                     </div>
                     
                     <aside className='details-item__column-right'>
                         <span className='details-item__label'>
-                            Nuevo -234 vendidos
+                            {TranslateItemStatus(item.condition)} - {item.sold_quantity} {item.sold_quantity > 1 ? "vendidos": "vendido"}
                         </span>
                         <h1 className='details-item__name'>
-                            Deco reverse sombrero dos lineas
+                            {item.title}
                         </h1>
                         <h2 className='details-item__price'>
                             <span className='details-item__price-symbol'>$</span>
-                            <span className='details-item__price-fraction'>1280</span>
-                            <img src="" alt="" className='etails-item__shiping-free'/>
+                            {item.price ? <span className='details-item__price-fraction'>{item.price.amount}</span> : null}
+                            {/*  */}
+                            {item.free_shipping ? <img src={c_shipping} className='details-item__shiping-free' alt='icon-shiping-free'/> : null}
                         </h2>
                         <button className='details-item__buy'>
                             Comprar
@@ -35,12 +84,33 @@ const details = () => {
                         Description
                     </h3>
                     <p className='details-item__description-p'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aliquam sed autem iusto alias eum nulla suscipit adipisci aspernatur, dolores laboriosam maxime obcaecati? Fuga pariatur quos, voluptas ea nisi maxime.
+                       {item.description}
                     </p>
                 </div>
+            </>
+        );
+    }
+
+    function body() {
+        console.info(item)
+        if (isLoad) {
+            return renderLoading();
+        } else if (!isLoad && error) {
+            return renderError()
+        }
+        else if (!isLoad && !error && item) {
+            return renderDetail()
+        }
+    }
+
+    return ( 
+        <section className='max-width-element details-item'>
+            <BreadCrumb crumbs={[{ name: item.title, url: `/items/${id}` }]}/>
+            <div className='details-item__wrapper'>
+                {body()}
             </div>
         </section>
     )
 }
 
-export default details;
+export default Details;
